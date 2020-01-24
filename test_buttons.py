16e42e0db6c85d4d47ecfaa8220273272b237bf5
Ian Ingram <iingram@sandiegozoo.org>
2020-01-23 16:04:23 -0800

    Adding program to test the functioning of the GPIO-pin-connected buttons on the Adafruit PiTFT.

---
+++ test_buttons.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    buttonState_0 = GPIO.input(17)
+    buttonState_1 = GPIO.input(22)
+    buttonState_2 = GPIO.input(23)
+    buttonState_3 = GPIO.input(27)
+    
+    print("Button Output:" +
+          str(buttonState_0) +
+          str(buttonState_1) +
+          str(buttonState_2) +
+          str(buttonState_3))
+    
+    time.sleep(.3)
