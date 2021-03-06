eae9c52dc8aaf341f450d5263c0bb14cdd972a5d
Ian Ingram <iingram@sandiegozoo.org>
2021-03-06 13:00:59 -0800

    getting the required packages in here

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ import setuptools
 
 setuptools.setup(
     name="dencam",
-    author="Ian Ingram",
-    version="0.0.1",
-    author_email="iingram@sandiegozoo.org",
+    author="Conservation Technology Lab at the San Diego Zoo Wildlife Alliance",
     description="Code for MiniDencam: Polar bear maternal den observation system.",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'pyyaml',
+        'rpi.gpio',
+        'picamera',
+        'netifaces',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
 )
