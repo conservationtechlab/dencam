15c6cb04d5c059bc9565c3e2d081827829a6ff6b
Ian Ingram <iingram@sandiegozoo.org>
2021-05-10 12:58:26 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 import os
-from setuptools import setup
+import re
 
-PATH = os.path.abspath(os.path.dirname(__file__))
+import setuptools
 
-with open(os.path.join(PATH, "README.md")) as f:
-    README_TEXT = f.read()
 
+def read(filename):
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+    with open(path , 'r') as f:
+        return f.read()
+    
+
+def find_version(text):
+    match = re.search(r"^__version__\s*=\s*['\"](.*)['\"]\s*$", text,
+                      re.MULTILINE)
+    return match.group(1)
+    
+    
 AUTHOR = "Conservation Technology Lab at the San Diego Zoo Wildlife Alliance"
 DESC = "Code for MiniDencam: Polar bear maternal den observation system."
 
-setup(
+setuptools.setup(
     name="dencam",
-    version="0.0.2",
+    version=find_version(read('dencam/__init__.py')),
     author=AUTHOR,
     description=DESC,
-    long_description=README_TEXT,
+    long_description=read('README.md'),
     long_description_content_type="text/markdown",
     url="https://github.com/icr-ctl/dencam",
     license="MIT",
@@ -29,6 +39,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        'Development Status :: 3 - Alpha',
         'Topic :: Scientific/Engineering',
     ],
     entry_points={"console_scripts": ["dencam=dencam.__main__:main"]},
