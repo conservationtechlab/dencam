ab0dcec12caea41adfd69a1d1e7ad4844c4889d5
iamgarcia <32944795+iamgarcia@users.noreply.github.com>
2021-12-28 11:21:19 -0800

    Add setup script to disable screen saver

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Disable screensaver and screen blanking
+# Disable screen saver blanking, turn off DPMS,
+# and disable blanking the video device
 sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart > /dev/null << EOF
 @xset s off
 @xset -dpms
@@ -10,6 +11,3 @@ EOF
 # Display the values of all current X Window
 # System preferences
 xset -q
-
-# Run setup.py to install Dencam dependencies
-python setup.py install
