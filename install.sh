f623ba6062af9ec16ca1b40c0f891802b953448d
iamgarcia <32944795+iamgarcia@users.noreply.github.com>
2022-01-11 13:44:59 -0800

    Add Adafruit PiTFT installer script to install.sh
    - Add Adafruit PiTFT installer script to install.sh
    - Add Adafruit PiTFT installer script to dencam repository

--- install.sh
+++ install.sh
@@ -12,3 +12,10 @@ EOF
 # Under the 'Screen Saver' section, 'prefer blanking' should be set to 'no'.
 # Under the 'DPMS (Energy Star)' section, it should say 'DPMS is Disabled'.
 xset -q
+
+# Download adafruit installer script dependencies.
+sudo pip3 install --upgrade adafruit-python-shell click
+
+# Run adafruit installer script. When it asks you to reboot, enter 'Y'
+# for yes. Rebooting ensures that the settings will take full effect. 
+sudo python3 adafruit-pitft.py --display=28r --rotation=90 --install-type=fbcp
