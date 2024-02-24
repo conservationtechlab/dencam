bec1857ee20167c605a3edcc0b5c4e1c4785ace6
Ian Ingram <ianishidden@gmail.com>
2024-02-24 06:11:42 -0800

    Get both CSV filepath and column to plot from CLI

--- utilities/solar_plot.py
+++ utilities/solar_plot.py
@@ -1,7 +1,27 @@
+"""Plot some useful data from solar logs
+
+Example usage: python solar_plot.py /home/pi/dencam/solar.csv
+
+"""
+import argparse
 import sys
 
 import matplotlib.pyplot as plt
 import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f',
+                    '--filepath',
+                    required=True,
+                    help="Path to solar data CSV file.")
+parser.add_argument('-c',
+                    '--column',
+                    required=True,
+                    help="Column of CSV to plot.")
+args = parser.parse_args()
+
+file_path = args.filepath
+column_to_plot = args.column
 
 
 def _get_x_axis_label(date, time):
@@ -11,27 +31,19 @@ def _get_x_axis_label(date, time):
     return date_time_label
 
 
-def _get_argument():
-    return sys.argv[1]
-
-
-file_path = "/home/pi/dencam/solar.csv"  # path to solar goes here
-
+data_frame = pd.read_csv(file_path)
 
 plt.rcParams["figure.figsize"] = [800, 6]
 plt.rcParams["figure.autolayout"] = True
 plt.xticks(rotation=90)
 plt.rc('axes', labelsize=5)
 
-data_frame = pd.read_csv(file_path)
 date_list = data_frame["Date"].tolist()
 time_list = data_frame["Time"].tolist()
 date_time_label = _get_x_axis_label(date_list, time_list)
 
-solar_data = str(_get_argument())
-
-plt.title(solar_data)
-plt.plot(date_time_label, data_frame[solar_data], 'g--', label=solar_data)
-plt.scatter(date_time_label, data_frame[solar_data], marker='o')
+plt.title(column_to_plot)
+plt.plot(date_time_label, data_frame[column_to_plot], 'g--', label=column_to_plot)
+plt.scatter(date_time_label, data_frame[column_to_plot], marker='o')
 plt.xticks(date_time_label[::5])
 plt.show()
