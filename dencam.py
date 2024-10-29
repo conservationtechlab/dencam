b94974402a7988f7bd984c855f488052ac38d39a
Ian Ingram <ianishidden@gmail.com>
2024-10-29 10:37:24 -0700

    Lint dencam.py

--- dencam.py
+++ dencam.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """Control code for polar bear maternal den observation device.
 
 The target hardware is a Raspberry Pi 4 Model B single board computer
@@ -19,7 +18,8 @@ import time
 import yaml
 from picamera.exc import PiCameraMMALError
 
-from dencam import logs, __version__
+from dencam import __version__
+from dencam.logs import setup_logger
 from dencam.buttons import ButtonHandler
 from dencam.recorder_picamera import PicameraRecorder
 from dencam.gui import ErrorScreen, Controller, State
@@ -31,20 +31,24 @@ parser.add_argument('config_file',
 args = parser.parse_args()
 
 LOGGING_LEVEL = logging.INFO
-log = logs.setup_logger(LOGGING_LEVEL)
+log = setup_logger(LOGGING_LEVEL)
 log.info('*** MINIDENCAM v%s STARTING UP ***', __version__)
 strg = logging.getLevelName(log.getEffectiveLevel())
 # clearly below line only reports for debug and info levels
-log.info('Logging level is {}'.format(strg))
+log.info("Logging level is %s", strg)
 
-with open(args.config_file) as f:
-    configs = yaml.load(f, Loader=yaml.SafeLoader)
+with open(args.config_file, 'r', encoding='utf8') as config_file:
+    configs = yaml.load(config_file, Loader=yaml.SafeLoader)
 log.info('Read in configuration settings')
 
 
 def main():
     flags = {'stop_buttons_flag': False}
-    STATE_LIST = ['OffPage', 'NetworkPage', 'RecordingPage', "SolarPage", "BlankPage"]
+    state_list = ['OffPage',
+                  'NetworkPage',
+                  'RecordingPage',
+                  "SolarPage",
+                  "BlankPage"]
 
     def cleanup(flags):
         flags['stop_buttons_flag'] = True
@@ -65,30 +69,27 @@ def main():
         if error_screen is not None:
             error_screen.hide()
 
-        number_of_states = len(STATE_LIST)
+        number_of_states = len(state_list)
         state = State(number_of_states)
         airplane_mode = AirplaneMode(configs)
         button_handler = ButtonHandler(recorder,
                                        state,
-                                       STATE_LIST,
+                                       state_list,
                                        airplane_mode,
                                        lambda: flags['stop_buttons_flag'])
-        button_handler.setDaemon(True)
+        button_handler.daemon = True
         button_handler.start()
 
-        controller = Controller(configs, recorder, STATE_LIST, state, airplane_mode)
-        controller.setDaemon(True)
+        controller = Controller(configs, recorder, state_list,
+                                state, airplane_mode)
+        controller.daemon = True
         controller.start()
 
-        while(True):
-            pass
+        while True:
             time.sleep(.1)
 
     except KeyboardInterrupt:
         log.debug('Keyboard interrupt received.')
-        cleanup(flags)
-    except Exception:
-        log.exception('An Exception in primary try block')
         cleanup(flags)
 
 
