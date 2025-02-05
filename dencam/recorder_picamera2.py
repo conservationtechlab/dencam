02e8f84e96f11d631174aa6f653b825771b5e3a7
kgarwoodsdzwa <kgarwood@sdzwa.org>
2025-02-05 14:54:01 -0800

    add timestamp to recording
    currently works but needs some work- ie fixing the black back
    ground to be same size as text size, and need to figure out how
    to not have the timestamp in the preview and just on the recorded
    video

--- dencam/recorder_picamera2.py
+++ dencam/recorder_picamera2.py
@@ -3,7 +3,10 @@
 """
 import logging
 from picamera2.encoders import H264Encoder
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2, Preview, MappedArray
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import time
 
 from dencam.recorder import Recorder
 
@@ -70,5 +73,40 @@ class Picamera2Recorder(Recorder):
         log.info('Set up camera per configurations')
         self.camera = Picam2(configs)
         self.configs = configs
-
         super().finish_setup()
+
+    def update_timestamp(self):
+        self.camera.camera.pre_callback = self._pre_callback_wrapper
+
+    def _pre_callback_wrapper(self, request):
+        """ Wrapper function to call update_timestamp without needing to pass request
+        """
+        self.timestamp(request)
+
+    def timestamp(self, request):
+        font_size = 35
+        text_color = (255, 255, 255)
+        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", font_size)
+        bg_color = (0, 0, 0)
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+
+        with MappedArray(request, "main") as m:
+            image = Image.fromarray(m.array)
+            draw = ImageDraw.Draw(image)
+
+            bbox = draw.textbbox((0, 0), timestamp, font=font)
+            text_width = bbox[2] - bbox[0]
+            text_height = bbox[3] - bbox[1]
+
+            img_width, img_height = image.size
+            text_x = (img_width - text_width) // 2
+            text_y = 10
+
+            draw.rectangle(
+                [text_x, text_y, text_x + text_width, text_y + text_height],
+                fill=bg_color
+            )
+
+            draw.text((text_x, text_y), timestamp, fill=text_color, font=font)
+
+            m.array[:] = np.array(image)
